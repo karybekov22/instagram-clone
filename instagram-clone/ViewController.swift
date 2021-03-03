b3165fbfc1c93023b7b09732710915c26efd67ea
Nursultan Karybekov <n.karybekov22@gmail.com>
2021-03-03 18:12:24 +0600

    Initial Commit

---
+++ instagram-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-clone
+//
+//  Created by Nursultan Karybekov on 3/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
