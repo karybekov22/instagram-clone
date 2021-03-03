0cff0e66dd1e8b9e5d999fceb8331f4af1b59e81
Nursultan Karybekov <n.karybekov22@gmail.com>
2021-03-03 18:54:28 +0600

    new commit

---
+++ instagram-clone/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  instagram-clone
+//
+//  Created by Nursultan Karybekov on 3/3/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
